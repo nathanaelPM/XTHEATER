5bfba8eb8c3e41b04918d19db1a80574b9177591
nathanaelPM <nathanael.pellico.cet146c4@gmail.com>
2020-05-08 18:39:16 -0500

    updated

--- xtheater/index.php
+++ xtheater/index.php
@@ -28,6 +28,7 @@ if(isset($_SESSION['login_user_sys'])){
 <!--header start here-->
 <div id="header">
 </div>
+
 <h1>Formulario de inicio de sesión</h1>
 <div class="header agile">
 	<div class="wrap">
